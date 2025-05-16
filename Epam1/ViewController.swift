a60b5941cd86156add8210f5f0e939f202b2342f
d1onnisss <lim_al@auca.kg>
2025-05-16 22:01:13 +0600

    Initial Commit

---
+++ Epam1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Epam1
+//
+//  Created by Alexey Lim on 16/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
